03ec0a91509c3dc38392fdef805dcc4850eb2844
hrwillmott <indihank@gmail.com>
2023-03-29 16:35:46 +0100

    additioons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ export default function Header() {
   return (
     <header className='bg-[#222222] flex-col justify-center text-center'>
       <img className='mx-auto py-4' src={logoRekordbox}></img>
-      <h1 className='text-white font-bold text-5xl pb-2'>Setlist Export</h1>
-      <h2 className='font-bold text-2xl text-[#027DE1] pt-2 pb-6 uppercase'>upload and transform your .cue file into a readable setlist</h2>
+      <h1 className='text-white font-bold text-5xl pb-2'>Rekordbox CUE file Converter</h1>
+      <h2 className='font-bold text-2xl text-[#027DE1] pt-2 pb-6 uppercase'>upload and convert your .cue files a setlist</h2>
     </header>
   )
 }
